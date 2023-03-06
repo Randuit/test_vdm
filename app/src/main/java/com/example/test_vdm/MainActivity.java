7fbf950ef692222b077ecc6357acceb66b433c34
Randuit <ste.delfini01@gmail.com>
2023-03-06 16:47:52 +0100

    v0.1

--- app/src/main/java/com/example/test_vdm/MainActivity.java
+++ app/src/main/java/com/example/test_vdm/MainActivity.java
@@ -3,12 +3,74 @@ package com.example.test_vdm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> userList;
+    Handler mainHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    class fetchData extends Thread{
+
+        String data = "";
+
+        public void run(){
+
+            mainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    
+                }
+            });
+
+            try {
+                URL url = new URL("https://vidiemme-recruitmenttask-webapi-dev.swarm.vidiemme.it/api/v1/employees");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    data = data + line;
+                }
+
+                if (!data.isEmpty()){
+                    JSONObject jsonObject = new JSONObject(data);
+                    JSONArray users = jsonObject.getJSONArray("data");
+                    userList.clear();
+                    for(int i=0;i<users.length();i++){
+                        JSONObject names = users.getJSONObject(i);
+                        String name = names.getString("name");
+                        userList.add(name);
+                    }
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
